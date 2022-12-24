70bea35bf267392089271903d95868311322c486
Kevin Dam <kevinqdam@gmail.com>
2022-12-23 21:06:15 -0800

    Add technology pills, custom anchor, experinece in progress

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,4 +1,26 @@
+import Anchor from "../components/Anchor";
 import Container from "../components/Container";
+
+type TechnologyListProps = {
+  company: string;
+  technologies: readonly string[];
+};
+
+const TechnologyPills: React.FC<TechnologyListProps> = ({
+  company,
+  technologies,
+}) => (
+  <ul className="flex flex-row">
+    {technologies.map((tech) => (
+      <li
+        key={`${company}-${tech}`}
+        className="mr-2 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-semibold text-blue-800 dark:bg-teal-200 dark:text-teal-800"
+      >
+        {tech}
+      </li>
+    ))}
+  </ul>
+);
 
 const Experience: React.FC = () => (
   <Container>
@@ -13,7 +35,11 @@ const Experience: React.FC = () => (
           <p className="col-span-1 text-sm">June 2022 - Present</p>
           <div className="col-span-2">
             <p className="text-sm">Software Development Engineer</p>
-            <p className="text-sm">@ Amazon Web Services</p>
+            <p className="pb-1 text-sm">@ Amazon Web Services</p>
+            <TechnologyPills
+              company="AWS"
+              technologies={["TypeScript", "AWS"]}
+            />
           </div>
         </div>
         <div className="grid grid-cols-3 gap-2 pb-6">
@@ -21,9 +47,22 @@ const Experience: React.FC = () => (
           <div className="col-span-2 flex flex-col">
             <p className="text-sm">Software Engineer</p>
             <p className="pb-1 text-sm">@ ServiceNow</p>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
-              ServiceNow, I did a lot of stuff here
+            <p className="pb-1 text-sm text-gray-600 dark:text-gray-400">
+              Developed features for{" "}
+              <Anchor href="https://docs.servicenow.com/bundle/sandiego-platform-user-interface/page/get-started/servicenow-overview/concept/next-experience-ui.html">
+                Next Experience UI (Polaris)
+              </Anchor>{" "}
+              in the San Diego release of ServiceNow, finally delivering it
+              after its delay for two releases. Part of the team that delivered{" "}
+              <Anchor href="https://docs.servicenow.com/en-US/bundle/tokyo-application-development/page/script/JavaScript-engine-upgrade/concept/c_JS_engine_upgrade.html">
+                ECMAScript 2021
+              </Anchor>{" "}
+              in the Tokyo release of ServiceNow.
             </p>
+            <TechnologyPills
+              company="ServiceNow"
+              technologies={["Java", "JavaScript", "Now Experience Framework"]}
+            />
           </div>
         </div>
         <div className="grid grid-cols-3 gap-2 pb-6">
@@ -31,9 +70,13 @@ const Experience: React.FC = () => (
           <div className="col-span-2 flex flex-col">
             <p className="text-sm">Implementation Consultant</p>
             <p className="pb-1 text-sm">@ Fast Enterprises</p>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
+            <p className="pb-1 text-sm text-gray-600 dark:text-gray-400">
               Fast, I did a lot of stuff here
             </p>
+            <TechnologyPills
+              company="Fast Enterprises"
+              technologies={["Microsoft SQL Server", "VB .NET"]}
+            />
           </div>
         </div>
         <div className="grid grid-cols-3 gap-2 pb-6">
