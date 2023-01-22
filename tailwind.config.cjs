ef6ffbf034e1876ba8b1987a11040959e2a8bd2f
Kevin Dam <kevinqdam@gmail.com>
2023-01-22 15:38:13 -0800

    Switch to Source Sans Pro font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
