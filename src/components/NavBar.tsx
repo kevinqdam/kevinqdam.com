ceffd6260d2db9ec760e517f27faa21737d8dbd5
Kevin Dam <kevinqdam@gmail.com>
2023-01-22 22:44:10 -0800

    change dismiss mobile menu button icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -45,6 +45,23 @@ const MoonIcon = (
   </svg>
 );
 
+const circledXIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth='1.5'
+    stroke='currentColor'
+    className='w-8 h-8'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+    />
+  </svg>
+);
+
 const NavItem: React.FC<PropsWithChildren<NavItemProps>> = ({
   activePath,
   path,
@@ -184,20 +201,7 @@ const NavBar: React.FC<NavBarProps> = ({ activePath }) => {
               onClick={handleMenuBtnClick}
               className='flex flex-row justify-center pt-20'
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='w-6 h-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M6 18L18 6M6 6l12 12'
-                />
-              </svg>
+              {circledXIcon}
             </button>
           </ul>
         )}
